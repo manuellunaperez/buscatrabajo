2c49ac4987469c667f70a498e02258978ff20a3c
ManuelLunaPerez <manuellunaperez@gmail.com>
2014-06-10 19:15:32 +0200

    Prueba2

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -9,6 +9,64 @@ def nameindex(name='Stranger'):
 def index():
     return template('index.tpl')
 
+@route('/busqueda')
+def busqueda():
+	provincia = bottle.request.forms.get("provincia")
+	if len(provincia) > 1:
+		provincia = "province=%s" % provincia
+	else:
+		provincia = ""
+	categoria = bottle.request.forms.get("categoria")
+	if len(categoria) > 1:
+		categoria = "&subcategory=%s" % categoria
+	else:
+		categoria = ""
+	contratos = bottle.request.forms.get("contratos")
+	if len(contratos) > 1:
+		contratos = "&contractType=%s" % contratos
+	else:
+		contratos = ""
+	formacion = bottle.request.forms.get("formacion")
+	if len(formacion) > 1:
+		formacion = "&study=%s" % formacion
+	else:
+		formacion = ""
+	oferta = "https://api.infojobs.net/api/1/offer?"
+	url = "%s%s%s%s%s" % (oferta,provincia,categoria,contratos,formacion)
+	listatitulos = []
+	listaciudad = []
+	listanombreempresa = []
+	listaexperiencia = []
+	listajornada = []
+	listaestudios = []
+	listarequisitosmin = []
+	listalink = []
+	f = requests.get(url)
+	archivo = json.load(f)
+	ofertas = archivo["offers"]	
+	for oferta in ofertas:
+		titulo = oferta["title"]
+		ciudad = oferta["city"]
+		nombreempresa = oferta["author"]["name"]
+		experienciaminima = oferta["experienceMin"]["value"]
+		jornada = oferta["workDay"]["value"]
+		estudios = oferta["study"]["value"]
+		requisitosmin = oferta["requirementMin"]
+		link = oferta["link"]
+		listatitulos.append(titulo)
+		listaciudad.append(ciudad)
+		listanombreempresa.append(nombreempresa)
+		listaexperiencia.append(experienciaminima)
+		listajornada.append(jornada)
+		listaestudios.append(estudios)
+		listarequisitosmin.append(requisitosmin)
+		listalink.append(link)
+	
+		
+	
+	return template('respuesta.tpl', {'ofertas':ofertas,'titulo':listatitulos,'ciudad':listaciudad,'nombreempresa':listanombreempresa,'experienciaminima':listaexperiencia,'jornada':listajornada,'estudios':listaestudios,'requisitosmin':listarequisitosmin,'link':listalink})
+
+
 # This must be added in order to do correct path lookups for the views
 import os
 from bottle import TEMPLATE_PATH
