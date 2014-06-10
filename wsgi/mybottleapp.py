079915f9552dfdd7522cf38540f2bfa0d0faf7a8
ManuelLunaPerez <manuellunaperez@gmail.com>
2014-06-10 19:02:36 +0200

    añadido index

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -1,12 +1,13 @@
-from bottle import route, default_app
-
+# -*- coding: utf-8 -*-
+import os
+from bottle import route, run, template, get, post, request, response, redirect, default_app, static_file, TEMPLATE_PATH, error
 @route('/name/<name>')
 def nameindex(name='Stranger'):
     return '<strong>Hello, %s!</strong>' % name
  
 @route('/')
 def index():
-    return '<strong>Hello World!</strong>'
+    return template('index.tpl')
 
 # This must be added in order to do correct path lookups for the views
 import os
