79ef9b88005d44d3d92049ff31b1002370f558c2
ManuelLunaPerez <manuellunaperez@gmail.com>
2014-06-11 18:37:31 +0200

    Prueba auth

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -4,13 +4,18 @@ from bottle import route, run, template, get, post, request, response, redirect,
 import bottle
 import requests
 import json
-import httplib
 
-import base64
-import string
+
+
 auth = base64.encodestring('d02252860f40443b8afb68ad26137f3c:FTmld3Q4q167v3e8ZY2yZar/GRl8Jgm1QKi4IS7vNfTQk7E6YP')
 webservice.putheader("Authorization", "Basic %s" % auth)
 
+import urllib2, base64
+
+request = urllib2.Request("http://api.foursquare.com/v1/user")
+base64string = base64.encodestring('d02252860f40443b8afb68ad26137f3c:FTmld3Q4q167v3e8ZY2yZar/GRl8Jgm1QKi4IS7vNfTQk7E6YP')
+request.add_header("Authorization", "Basic %s" % base64string)   
+result = urllib2.urlopen(request)
 
 @get('/')
 def inicio():
