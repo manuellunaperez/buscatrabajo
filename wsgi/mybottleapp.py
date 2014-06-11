89dc821563b4b640dd6bd3d322421469155b05ea
ManuelLunaPerez <manuellunaperez@gmail.com>
2014-06-11 21:49:43 +0200

    Prueba auth

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -56,7 +56,7 @@ def busqueda():
 	client = "ZDAyMjUyODYwZjQwNDQzYjhhZmI2OGFkMjYxMzdmM2M6RlRtbGQzUTRxMTY3djNlOFpZMnlaYXIvR1JsOEpnbTFRS2k0SVM3dk5mVFFrN0U2WVA="
 	headers = {"Authorization" : "Basic %s" % client}
 	conn = httplib.HTTPConnection("api.infojobs.net")
-	conn.request("GET", "/api/1/offer?%s%s%s%s%s" % (oferta,provincia,categoria,contratos,formacion), headers=headers)
+	conn.request("GET", "/api/1/offer?%s%s%s%s" % (provincia,categoria,contratos,formacion), headers=headers)
 	response = conn.getresponse()
 	archivo = response.read()
 	conn.close()
