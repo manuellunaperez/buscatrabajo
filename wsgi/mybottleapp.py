ff453091da39646f3be121cd2009e0fde92b3030
ManuelLunaPerez <manuellunaperez@gmail.com>
2014-06-23 19:24:11 +0200

    Arreglo respuesta

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -89,13 +89,12 @@ def busqueda():
 		listarequisitosmin.append(requisitosmin)
 		listalink.append(link)
 	
-	return template('respuesta.tpl', {'ofertas':ofertas,'titulo':listatitulos,'ciudad':listaciudad,'nombreempresa':listanombreempresa,'experienciaminima':listaexperiencia,'jornada':listajornada,'estudios':listaestudios,'requisitosmin':listarequisitosmin,'link':listalink})
-	
+	return template('respuesta.tpl')
 
 @get('/datos.tpl')
 def inicio():
-    return template('datos.tpl')
- 
+    return template('datos.tpl'{'ofertas':ofertas,'titulo':listatitulos,'ciudad':listaciudad,'nombreempresa':listanombreempresa,'experienciaminima':listaexperiencia,'jornada':listajornada,'estudios':listaestudios,'requisitosmin':listarequisitosmin,'link':listalink})
+	
 import os
 from bottle import TEMPLATE_PATH
 ON_OPENSHIFT = False
